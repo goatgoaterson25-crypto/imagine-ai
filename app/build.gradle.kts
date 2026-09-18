ffc9706d34a31520f4d0033c1794916626a347e6
goatgoaterson25-crypto <goatgoaterson25@gmail.com>
2026-09-18 14:02:39 -0500

    Apollo: Injected build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.apollo.missioncontrol"
+    namespace = "com.apollo.imagine-ai"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.apollo.missioncontrol"
+        applicationId = "com.apollo.imagine-ai"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
